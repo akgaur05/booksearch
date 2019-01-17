5f227b2e8509f6cb067a492bc9a556f8a826e374
Akanksha Kapoor <agaur05@gmail.com>
2019-01-16 19:28:00 -0600

    final maybe 2

---
+++ client/build/precache-manifest.07dcdd81fe4c5b97f4b63879ac8811c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccd4288edf4e810f5e24",
+    "url": "/static/css/main.595aa477.chunk.css"
+  },
+  {
+    "revision": "ccd4288edf4e810f5e24",
+    "url": "/static/js/main.ccd4288e.chunk.js"
+  },
+  {
+    "revision": "f3135bfb86c700f725a2",
+    "url": "/static/js/1.f3135bfb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93b3866ed3cb4c9286429bb1dce3c6c7",
+    "url": "/index.html"
+  }
+];
