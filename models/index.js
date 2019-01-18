a63f170e587cd48c66d015e633fd378f70d5c8ed
Akanksha Kapoor <agaur05@gmail.com>
2019-01-17 18:46:52 -0600

    maybe we fix

--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
-module.exports = {
-    Book: require("./Book.js"),
-  };
+module.exports={
+    Book: import("./Book"),
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const db = require("../models/Book");
+const db = require("../models");
 
 router.get("/books", (req, res) => {
     db.Book.find({})
     .then(function(books) {
         res.status(200).json(books);
     })
-})
+});
 router.post("/books", (req, res) => {
     const info = {title: req.body.title, authors: req.body.authors, synopsis: req.body.synopsis,
     thumbnail: req.body.thumbnail, link: req.body.link}
@@ -14,13 +14,13 @@ router.post("/books", (req, res) => {
     .then(function(book) {
         res.status(200).json(book);
     }).catch(err => console.log(err))
-})
+});
 router.delete("/books/:id", (req,res) => {
     const id = req.params.id;
     db.Book.findByIdAndDelete({_id:id})
     .then(function(response) {
         res.status(200).json(response);
     })
-})
+});
 
 module.exports = router;
